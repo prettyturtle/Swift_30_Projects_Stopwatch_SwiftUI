d1a834f03d1b5ac00d392099f49aae2838b4e441
prettyturtle <unifconti@gmail.com>
2023-04-30 16:36:57 +0900

    Initial Commit

---
+++ Swift_30_Projects_Stopwatch/Swift_30_Projects_StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_StopwatchApp.swift
+//  Swift_30_Projects_Stopwatch
+//
+//  Created by yc on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
